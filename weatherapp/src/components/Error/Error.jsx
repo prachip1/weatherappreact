1007f1a4dfcfe807d16e47fc583a1bf6034326e0
Prachi P <prachip1908@gmail.com>
2022-02-06 12:09:47 +0530

    initial commit

---
+++ weatherapp/src/components/Error/Error.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import styles from "./Error.module.css";
+
+const Error = ({ message }) => (
+
+    <div className ={`${styles.error} alert position-absolute`} role="alert">
+         {message}
+    </div>
+);
+
+Error.propTypes ={
+    message: PropTypes.string,
+};
+
+Error.defaultProps = {
+    message: 'An error has occured',
+};
+
+export default Error;
